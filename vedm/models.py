785b592f68c91108104d6a35919344ebd9c5b758
Viktor Eikman <viktor.eikman@gmail.com>
2016-08-07 15:35:19 +0200

    parent_relationship, UploadableListMixin.

--- vedm/models.py
+++ vedm/models.py
@@ -21,40 +21,79 @@ class UploadableMixin():
     '''A mix-in for making Django models text-based.'''
 
     @classmethod
+    @transaction.atomic
     def create_en_masse(cls, raws):
+        '''Create a mass of objects from raw data out of text files.
+
+        This is only complicated because hierarchical relationships between
+        objects are held back until all objects have been registered. This
+        feature requires the field pointing to a parent to be named
+        "parent_object", for the model to have a "slug" field, and for
+        references to parents to be stored in text as strings that, when
+        slugified, correspond to genuine parent slugs.
+
+        '''
+
         logging.debug('Instantiating {} en masse.'.format(cls))
 
-        with transaction.atomic():
-            by_pk = dict()
+        by_pk = dict()
+        for raw_data, instance in cls._iterate_over_raw_data(raws):
+            by_pk[instance.pk] = raw_data
 
-            for item in raws:
-                new = cls.create(**item)
-                by_pk[new.pk] = item
+        # Add parents, which we presume are now saved.
+        for pk, item in by_pk.items():
+            sluggable = item.get('parent_object')
+            if sluggable:
+                slug = util.misc.slugify(sluggable)
+                try:
+                    parent = cls.objects.get(slug=slug)
+                except cls.DoesNotExist:
+                    s = 'Stated parent "{}" not found.'
+                    logging.error(s.format(sluggable))
+                    continue
+                child = cls.objects.get(pk=pk)
+                child.parent_object = parent
+                child.save()
 
-            # Add parents, which we presume are now saved.
-            for pk, item in by_pk.items():
-                sluggable = item.get('parent_object')
-                if sluggable:
-                    slug = util.misc.slugify(sluggable)
-                    try:
-                        parent = cls.objects.get(slug=slug)
-                    except cls.DoesNotExist:
-                        s = 'Stated parent "{}" not found.'
-                        logging.error(s.format(sluggable))
-                        continue
-                    child = cls.objects.get(pk=pk)
-                    child.parent_object = parent
-                    child.save()
+                if hasattr(child, 'parent_relationship'):
+                    if not child.parent_relationship:
+                        s = '"{}" has no relationship to its stated parent.'
+                        logging.error(s.format(child))
+
+    @classmethod
+    def _iterate_over_raw_data(cls, raws):
+        '''Assume each object in the raws describes one instance.'''
+        for item in raws:
+            new = cls.create(**item)
+            yield item, new
 
     @classmethod
     def create(cls, title='', parent_object=None, tags=None, **kwargs):
-        '''Ignore parent object, because it may not be saved yet.'''
+        '''Ignore parent object, because it may not be saved yet.
+
+        Assume that any tags are to be managed as if by Taggit.
+
+        '''
 
         slug = util.misc.slugify(title)
         new = cls.objects.create(title=title, slug=slug, **kwargs)
         if tags:
             new.tags.set(*tags)
         return new
+
+
+class UploadableListMixin(UploadableMixin):
+    '''A variation for records kept within lists.'''
+
+    @classmethod
+    def _iterate_over_raw_data(cls, raws):
+        '''An override.
+
+        Assume each object in the raws is a flat list describing one instance.
+
+        '''
+        for iterable in raws:
+            yield from super()._iterate_over_raw_data(iterable)
 
 
 class Document(models.Model, UploadableMixin):
