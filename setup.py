171934b4094f356e83aa1763061bf965db79a6c9
Viktor Eikman <viktor.eikman@gmail.com>
2016-08-28 16:58:34 +0200

    Made the management command more customizable.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(name='vedm',
-      version='0.5.0',
+      version='0.5.1',
       author='Viktor Eikman',
       author_email='viktor@eikman.se',
       description='Reusable Django miscellania',
--- vedm/management/commands/resolve_markup.py
+++ vedm/management/commands/resolve_markup.py
@@ -10,41 +10,44 @@ import vedm
 
 
 class Command(vedm.management.misc.LoggingLevelCommand):
+    '''A command that is useful out of the box but can be customized.'''
+
     help = 'Resolves all markup in special text fields into HTML.'
 
     @transaction.atomic
     def handle(self, *args, **kwargs):
         super().handle(**kwargs)
+        vedm.util.traverse.site(self.treat_app)
+
+    def treat_app(self, app):
+        vedm.util.traverse.app(self.treat_model, app)
+
+    def get_field_classes(self):
+        return (vedm.models.MarkupField,)
+
+    def treat_model(self, model):
+        fields = vedm.util.markup.get_fields(model, self.get_field_classes())
+
+        def treat_instance(instance):
+            vedm.util.traverse.instance(self.treat_field, instance, fields)
+            instance.save()
+
+        if fields:
+            vedm.util.traverse.model(treat_instance, model)
+
+    def treat_field(self, instance, string):
+        '''Manipulate a string, which is the content of a text field.'''
+        string = vedm.util.file.unwrap_paragraphs(string)
+
+        kwargs = dict()
 
         try:
             pass_instance = settings.MARKUP_INSTANCE_AS_SUBJECT
         except AttributeError:
             pass_instance = True
 
-        field_classes = (vedm.models.MarkupField,)
+        if pass_instance:
+            kwargs = dict(subject=instance)
 
-        def treat_field(instance, string):
-            '''Manipulate a string, which is the content of a text field.'''
-            string = vedm.util.file.unwrap_paragraphs(string)
-
-            kwargs = dict()
-            if pass_instance:
-                kwargs = dict(subject=instance)
-
-            string = vedm.util.markup.all_on_string(string, **kwargs)
-            return string
-
-        def treat_model(model):
-            fields = vedm.util.markup.get_fields(model, field_classes)
-
-            def treat_instance(instance):
-                vedm.util.traverse.instance(treat_field, instance, fields)
-                instance.save()
-
-            if fields:
-                vedm.util.traverse.model(treat_instance, model)
-
-        def treat_app(app):
-            vedm.util.traverse.app(treat_model, app)
-
-        vedm.util.traverse.site(treat_app)
+        string = vedm.util.markup.all_on_string(string, **kwargs)
+        return string
