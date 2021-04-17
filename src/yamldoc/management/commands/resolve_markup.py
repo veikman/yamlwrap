dce4146624a52cdc20f4aa33661ca32f057fe222
Viktor Eikman <viktor.eikman@gmail.com>
2021-04-17 21:13:48 +0200

    Command cleanup.
    Bumped Python version requirement over a walrus.
    
    Little bit of generalization for string treatment, for compatibility
    with templating systems other than `ovid`.

--- src/yamldoc/management/commands/resolve_markup.py
+++ src/yamldoc/management/commands/resolve_markup.py
@@ -6,42 +6,48 @@
 from django.conf import settings
 from django.db import transaction
 
-import yamldoc
+from yamldoc.management.misc import LoggingLevelCommand
+from yamldoc.models import MarkupField
+from yamldoc.util.file import unwrap_paragraphs
+from yamldoc.util.markup import all_on_string, get_fields
+from yamldoc.util.traverse import app as traverse_app
+from yamldoc.util.traverse import instance as traverse_instance
+from yamldoc.util.traverse import model as traverse_model
+from yamldoc.util.traverse import site as traverse_site
 
 
-class Command(yamldoc.management.misc.LoggingLevelCommand):
+class Command(LoggingLevelCommand):
     """A command that is useful out of the box but can be customized."""
 
     help = 'Resolves all markup in special text fields into HTML.'
 
     @transaction.atomic
     def handle(self, *args, **kwargs):
-        """Handle command."""
+        """Handle CLI command."""
         super().handle(**kwargs)
-        yamldoc.util.traverse.site(self.treat_app)
+        traverse_site(self.treat_app)
 
     def treat_app(self, app):
         """Treat all models in passed app."""
-        yamldoc.util.traverse.app(self.treat_model, app)
+        traverse_app(self.treat_model, app)
 
     def get_field_classes(self):
         """Return an iterable of subject field types."""
-        return (yamldoc.models.MarkupField,)
+        return (MarkupField,)
 
     def treat_model(self, model):
         """Treat relevant fields on passed model."""
-        fields = yamldoc.util.markup.get_fields(model, self.get_field_classes())
-        if fields:
-            yamldoc.util.traverse.model(self.treat_instance, model, fields)
+        if fields := get_fields(model, self.get_field_classes()):
+            traverse_model(self.treat_instance, model, fields)
 
     def treat_instance(self, instance, fields):
         """Treat passed instance of a model."""
-        yamldoc.util.traverse.instance(self.treat_field, instance, fields)
+        traverse_instance(self.treat_field, instance, fields)
         instance.save()
 
-    def treat_field(self, instance, string):
+    def treat_field(self, instance, string: str, treatment=all_on_string):
         """Manipulate a string, which is the content of a text field."""
-        string = yamldoc.util.file.unwrap_paragraphs(string)
+        string = unwrap_paragraphs(string)
 
         try:
             pass_instance = settings.MARKUP_INSTANCE_AS_SUBJECT
@@ -52,5 +58,4 @@ class Command(yamldoc.management.misc.LoggingLevelCommand):
         if pass_instance:
             kwargs.update(subject=instance)
 
-        string = yamldoc.util.markup.all_on_string(string, **kwargs)
-        return string
+        return treatment(string, **kwargs)
