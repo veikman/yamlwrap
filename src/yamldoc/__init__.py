30fb6e4bfaff1836f3228437b944f152ed64c3db
Viktor Eikman <viktor.eikman@gmail.com>
2021-04-19 06:58:00 +0200

    Stopped auto-importing resolve_markup.

--- src/yamldoc/__init__.py
+++ src/yamldoc/__init__.py
@@ -18,7 +18,4 @@ this program. If not, see <https://www.gnu.org/licenses/>.
 
 """
 
-import yamldoc.management as management
-
-__all__ = ('management',)
 __version__ = '1.0.0-SNAPSHOT'
--- src/yamldoc/management/commands/__init__.py
+++ src/yamldoc/management/commands/__init__.py
@@ -1,23 +1,6 @@
-"""yamldoc.management.commands: Markup resolution management command.
+"""yamldoc.management.commands: Prefabricated management commands.
 
-Author: Viktor Eikman <viktor.eikman@gmail.com>
-
--------
-
-This program is free software: you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free Software
-Foundation, either version 3 of the License, or (at your option) any later
-version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE. See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with
-this program. If not, see <https://www.gnu.org/licenses/>.
+The contents of this package are not auto-imported because sites may wish to
+use yamldoc’s lower-level utilities instead.
 
 """
-
-from . import resolve_markup
-
-__all__ = ('resolve_markup',)
